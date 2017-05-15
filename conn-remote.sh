25f59b23f6f4e3d931416c5caf906c030c0564db
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-05-15 20:43:25 +0200

    store output in /dev/shm for later lookup

--- conn-remote.sh
+++ conn-remote.sh
@@ -1,2 +1,2 @@
-cat conn.log | bro-cut -d id.orig_h  | grep -v 193.198.21[2345] | sort | uniq -c | sort -rn | less
+cat conn.log | bro-cut -d id.orig_h  | grep -v 193.198.21[2345] | sort | uniq -c | sort -rn | tee /dev/shm/conn-remote.log | less
 
