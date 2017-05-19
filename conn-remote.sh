8c0281ea72802b25cfd217d78890ff673d1a9b44
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-05-19 21:22:15 +0200

    ignore more local addresses, more columns

--- conn-remote.sh
+++ conn-remote.sh
@@ -1,2 +1,3 @@
-cat conn.log | bro-cut -d id.orig_h  | grep -v 193.198.21[2345] | sort | uniq -c | sort -rn | tee /dev/shm/conn-remote.log | less
+show="id.orig_h id.resp_h id.resp_p proto"
+cat /var/log/bro/current/conn.log | bro-cut -d $show | grep -v '^193.198.21[2345]' | grep -v '^10\.' | sort | uniq -c | sort -rn | tee /dev/shm/conn-remote.log | less
 
