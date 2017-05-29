bb9a72d61db9596694e61a7befcd7521c387681a
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-05-29 18:32:51 +0200

    work in current or archive (.gz) dirs

--- software-count.sh
+++ software-count.sh
@@ -1,1 +1,1 @@
-zcat software.* | bro-cut -d software_type name version.major   version.minor   version.minor2  version.minor3 | sort | uniq -c | sort -n | tee /dev/shm/software.count 
+zcat software.* || cat software.log | bro-cut -d software_type name version.major   version.minor | sort | uniq -c | sort -n | tee /dev/shm/software.count 
