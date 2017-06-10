66b6e2b8453edcfede78bfec6b71e2f72736fda0
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-06-10 07:29:26 +0200

    transfer expire time

--- site/firewall.sh
+++ site/firewall.sh
@@ -2,9 +2,11 @@
 
 while true ; do
 	redis-cli -n 4 --scan | while read ts ; do
-		ip=`echo GET $ts | redis-cli -n 4 | tee /dev/shm/4.json | jq .orig_h`
-		echo " $ip "
-		ssh -i /home/dpavlin/.ssh/mtik/enesej enesej@193.198.212.1 "/ip firewall address-list add list=public_blacklist address=$ip timeout=1h comment=Blocked-by-BRO"
+		json=`echo GET $ts | redis-cli -n 4 | tee /dev/shm/4.json`
+	       	ip=`echo $json | jq .orig_h`
+		expire=`echo $json | jq .expire`
+		echo " $ip/$expire "
+		ssh -i /home/dpavlin/.ssh/mtik/enesej enesej@193.198.212.1 "/ip firewall address-list add list=public_blacklist address=$ip timeout=${expire}s comment=Blocked-by-BRO"
 		echo DEL $ts | redis-cli -n 4
 	done
 	echo -n .
