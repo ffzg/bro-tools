ba0d8e3637f6b1e5699365bd1eb89174060f8faa
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-07-04 19:32:24 +0200

    send events to user.log syslog

--- site/firewall.sh
+++ site/firewall.sh
@@ -9,6 +9,7 @@ while true ; do
 		echo " $ip/$expire $msg "
 		ssh -i /home/dpavlin/.ssh/mtik/enesej enesej@193.198.212.1 "/ip firewall address-list add list=public_blacklist address=$ip timeout=${expire}s comment=$msg"
 		echo DEL $ts | redis-cli -n 4
+		logger -f /dev/shm/4.json --tag=json --id=4
 	done
 	echo -n .
 	sleep 3
--- site/notices.sh
+++ site/notices.sh
@@ -10,6 +10,7 @@ while true ; do
 		echo " $ip/$expire $msg"
 		ssh -i /home/dpavlin/.ssh/mtik/enesej enesej@193.198.212.1 "/ip firewall address-list add list=public_blacklist address=$ip timeout=${expire}s comment=$msg"
 		echo DEL $ts | redis-cli -n 5
+		logger -f /dev/shm/5.json --tag=json --id=5
 	done
 	echo -n .
 	sleep $sleep
