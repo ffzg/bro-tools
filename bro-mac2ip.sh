a697c090ee3c6b115a0cd8c5fa2d6039e539944f
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-04-02 09:58:40 +0200

    grep mac from generated files
    single mac grep is faster than using zegrep, for multiple macs

--- bro-mac2ip.sh
+++ bro-mac2ip.sh
@@ -1,10 +1,23 @@
 #!/bin/sh -e
 
+dir=/var/log/bro/.mac2ip
+
+if [ ! -z "$1" -a "$1" != "update" ] ; then
+	if [ -z "$2" ] ; then
+		# just one mac, faster even for multiple invocations!
+		zgrep $1 $dir/*.gz
+	else
+		re=`echo $* | sed 's/ /|/g'`
+		zegrep "($re)" $dir/*.gz
+	fi
+	exit 0
+fi
+
+
 # ignore tilera and pix mac for nat returns
 # this hides public IPs which are not interesting
 filter="(d4:ca:6d:01:4c:f2|00:0e:0c:5f:77:1e)"
 
-dir=/var/log/bro/.mac2ip
 test -d $dir || mkdir $dir
 
 ls -t /var/log/bro/*/conn.*gz | while read log_path ; do
