5c7c4f5dcb52d1708a8770da2d80611936d1438d
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-04-13 22:11:49 +0200

    count used IP addresses

---
+++ bro-ips.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+old_date=''
+
+ls /var/log/bro/*/conn.*.log.gz | while read file ; do
+	name=$( basename $file )
+	date=$( echo $file | cut -d/ -f5 )
+	echo "# $date $name - $file"
+	if [ "$old_date" != "$date" ] ; then
+		test -d ips/$date || mkdir ips/$date
+		if [ ! -z "$old_date" ] ; then
+			echo "# create date $date summary"
+			cat ips/$old_date/* | sort -u > ips/$old_date.ips
+			wc -l ips/$old_date.ips
+		fi
+		old_date=$date
+	fi
+	zcat $file | bro-cut id.orig_h id.resp_h | sed 's/\t/\n/' | grep '193\.198\.21[2345]\.' | sort -u > ips/$date/$name
+	wc -l ips/$date/$name
+done
+
+exit 0
+
+bro-cut id.orig_h id.resp_h < /var/log/bro/current/conn.log  | sed 's/\t/\n/' | grep '193\.198\.21[2345]\.' | sort -u | ~/sort-ip.pl
