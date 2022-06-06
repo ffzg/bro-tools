4023feda7312fd22841d360f3b2b3dfb8243a358
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-06-06 15:11:50 +0200

    correctly grep ips with single digit last octet

--- bro-ips-grep.sh
+++ bro-ips-grep.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -e
 
-ip=193.198.213.175
-#ip=$1
+ip=$1
 
-grep $ip ./ips/*.ips | while read file ; do
+test -z "$ip" && echo "Usage: $0 ip" && exit 1
+
+grep --line-regex $ip ./ips/*.ips | while read file ; do
 	path=$( echo $file | sed 's/\.ips.*$//' )
 	echo "# $file -> $path"
-	grep $ip $path/* | while read file2 ; do
+	grep --line-regex $ip $path/* | while read file2 ; do
 		path2=$( echo $file2 | sed -e 's,\./ips,/var/log/bro,' -e "s/:$ip//" )
 		echo "## $file2 -> $path2"
 
-		zgrep $ip $path2
+		zgrep "$ip[^0-9]" $path2
 	done
 done
 
